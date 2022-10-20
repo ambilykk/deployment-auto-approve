1332459db23606314a251c0fea769da334b32df2
Ambily <ambilykk@Ambilys-MacBook-Pro.local>
2022-10-20 13:50:48 +0530

    Incorporating environment to pre-approve deployment

--- app/index.js
+++ app/index.js
@@ -6,6 +6,9 @@ const github = require('@actions/github');
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
+// get the environment to pre-approve deployment
+const envIn = core.getInput('environment');
+
 async function run() {
 
     try {
@@ -21,43 +24,45 @@ async function run() {
         let envReviewers = [];
         let isReviewer = false;
         response.data.forEach(env => {
-            env_id.push(env.environment.id);
-            env_name = env_name + env.environment.name + ',';
+            if (env.environment.includes(envIn)) {
+                env_id.push(env.environment.id);
+                env_name = env_name + env.environment.name + ',';
 
-            // check if the current user is a reviewer for the environment
-            env.reviewers.forEach(async reviewerObj => {
-                // If the reviewer is a User
-                if (reviewerObj.type == 'User' && !isReviewer) {
-                    envReviewers.push(reviewerObj.reviewer.login);
-                    if (reviewerObj.reviewer.login == github.context.actor) {
-                        isReviewer = true;
-                    }
-                }
-                // If the reviewer is a Team
-                if (reviewerObj.type == 'Team' && !isReviewer) {
-                    envReviewers.push(reviewerObj.reviewer.name);
-
-                    await octokit.rest.teams.getMembershipForUserInOrg({
-                        org: github.context.repo.owner,
-                        team_slug: reviewerObj.reviewer.slug,
-                        username: github.context.actor
-                    }).then((response) => {
-                        console.log(` team membership checked for ${github.context.actor} in team ${reviewerObj.reviewer.slug}`);
-                        console.log(` response: ${response.status}`);
-                        if (response.status == 200) {
+                // check if the current user is a reviewer for the environment
+                env.reviewers.forEach(async reviewerObj => {
+                    // If the reviewer is a User
+                    if (reviewerObj.type == 'User' && !isReviewer) {
+                        envReviewers.push(reviewerObj.reviewer.login);
+                        if (reviewerObj.reviewer.login == github.context.actor) {
                             isReviewer = true;
                         }
-                    }).catch((error) => {
-                        console.log(` team membership check failed for ${github.context.actor} in team ${reviewerObj.reviewer.name}`);
-                    });;
-                }
-            });
+                    }
+                    // If the reviewer is a Team
+                    if (reviewerObj.type == 'Team' && !isReviewer) {
+                        envReviewers.push(reviewerObj.reviewer.name);
+
+                        await octokit.rest.teams.getMembershipForUserInOrg({
+                            org: github.context.repo.owner,
+                            team_slug: reviewerObj.reviewer.slug,
+                            username: github.context.actor
+                        }).then((response) => {
+                            console.log(` team membership checked for ${github.context.actor} in team ${reviewerObj.reviewer.slug}`);
+                            console.log(` response: ${response.status}`);
+                            if (response.status == 200) {
+                                isReviewer = true;
+                            }
+                        }).catch((error) => {
+                            console.log(` team membership check failed for ${github.context.actor} in team ${reviewerObj.reviewer.name}`);
+                        });;
+                    }
+                });
+            }
         });
 
         // if the current user is not a reviewer, display the list of reviewers and exit
         if (!isReviewer) {
             // Writing to build log            
-            core.notice('Auto Approval Not Possible; current user is not a reviewer for the environment(s) - ' + env_name);
+            core.notice('Auto Approval Not Possible; current user is not a reviewer for the environment(s) - ' + env_name.trimEnd(','));
             core.info('Reviewers: ' + (envReviewers.join(',')));
             return;
         } else {
@@ -70,7 +75,7 @@ async function run() {
                     run_id: github.context.runId,
                     environment_ids: env_id,
                     state: 'approved',
-                    comment: 'Auto-Approved by GitHub Action for environment(s) - ' + env_name
+                    comment: 'Auto-Approved by GitHub Action for environment(s) - ' + env_name.trimEnd(',') 
                 });
                 // Adding to deployment Summary
                 core.summary.addHeading(' :white_check_mark: Auto Approval Status');
